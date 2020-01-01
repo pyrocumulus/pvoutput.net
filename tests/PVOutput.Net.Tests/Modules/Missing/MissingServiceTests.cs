8419b064c914387a19ac3019d9c9d6681d3e0f77
Marcel <dev@mboersma.nl>
2020-01-01 12:54:32 +0100

    Uri and separate object unit testing for Missing service

--- tests/PVOutput.Net.Tests/Modules/Missing/MissingServiceTests.cs
+++ tests/PVOutput.Net.Tests/Modules/Missing/MissingServiceTests.cs
@@ -1,51 +1,89 @@
-using System;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using PVOutput.Net.Objects.Factories;
+using PVOutput.Net.Objects.Modules;
 using PVOutput.Net.Tests.Utils;
+using RichardSzalay.MockHttp;
 
 namespace PVOutput.Net.Tests.Modules.Missing
 {
     [TestFixture]
-    public class MissingServiceTests
+    public partial class MissingServiceTests
     {
         [Test]
         public async Task MissingService_GetMissingDaysInPeriod_WithResult()
         {
-            var client = TestUtility.GetMockClient(MissingTestsData.GETMISSING_URL, MissingTestsData.MISSINGDATES_RESPONSE_SIMPLE);
+            var client = TestUtility.GetMockClient(out var testProvider);
+
+            testProvider.ExpectUriFromBase(GETMISSING_URL)
+                        .WithQueryString("df=20160801&dt=20160830")
+                        .RespondPlainText(MISSINGDATES_RESPONSE_SIMPLE);
+
             var response = await client.Missing.GetMissingDaysInPeriod(new DateTime(2016, 8, 1), new DateTime(2016, 8, 30));
+            testProvider.VerifyNoOutstandingExpectation();
 
-            if (response.Exception != null)
+            Assert.Multiple(() =>
             {
-                throw response.Exception;
-            }
+                Assert.IsNull(response.Exception);
+                Assert.IsTrue(response.HasValue);
+                Assert.IsNotNull(response.IsSuccess);
+                Assert.AreEqual(21, response.Value.Dates.Count());
 
-            Assert.IsTrue(response.HasValue);
-            Assert.IsNotNull(response.IsSuccess);
-
-            var missing = response.Value;
-            Assert.AreEqual(21, missing.Dates.Count());
-            Assert.AreEqual(new DateTime(2016, 8, 1), missing.Dates.First());
-            Assert.AreEqual(new DateTime(2016, 8, 21), missing.Dates.Last());
+            });
         }
 
         [Test]
         public async Task MissingService_GetMissingDaysInPeriod_NoResult()
         {
-            var client = TestUtility.GetMockClient(MissingTestsData.GETMISSING_URL, MissingTestsData.MISSINGDATES_RESPONSE_NONE);
-            var response = await client.Missing.GetMissingDaysInPeriod(new DateTime(2016, 8, 1), new DateTime(2016, 8, 30));
+            var client = TestUtility.GetMockClient(out var testProvider);
 
-            if (response.Exception != null)
+            testProvider.ExpectUriFromBase(GETMISSING_URL)
+                        .WithQueryString("df=20180301&dt=20180330")
+                        .RespondPlainText(MISSINGDATES_RESPONSE_NONE);
+
+            var response = await client.Missing.GetMissingDaysInPeriod(new DateTime(2018, 3, 1), new DateTime(2018, 3, 30));
+
+            Assert.Multiple(() =>
             {
-                throw response.Exception;
-            }
+                Assert.IsNull(response.Exception);
+                Assert.IsTrue(response.HasValue);
+                Assert.IsNotNull(response.IsSuccess);
+                Assert.IsNotNull(response.Value.Dates);
+                Assert.AreEqual(0, response.Value.Dates.Count());
+            });
+        }
 
-            Assert.IsTrue(response.HasValue);
-            Assert.IsNotNull(response.IsSuccess);
+        /*
+         * Deserialisation tests below
+         */
 
-            var missing = response.Value;
-            Assert.IsNotNull(missing.Dates);
-            Assert.AreEqual(0, missing.Dates.Count());
+        [Test]
+        public async Task MissingReader_ForResponse_CreatesCorrectObject()
+        {
+            var reader = StringFactoryContainer.CreateObjectReader<IMissing>();
+            IMissing result = await reader.ReadObjectAsync(new StringReader(MISSINGDATES_RESPONSE_SIMPLE));
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(21, result.Dates.Count());
+                Assert.AreEqual(new DateTime(2016, 8, 1), result.Dates.First());
+                Assert.AreEqual(new DateTime(2016, 8, 21), result.Dates.Last());
+            });
+        }
+
+        [Test]
+        public async Task MissingReader_ForEmptyResponse_CreatesCorrectObject()
+        {
+            var reader = StringFactoryContainer.CreateObjectReader<IMissing>();
+            IMissing result = await reader.ReadObjectAsync(new StringReader(MISSINGDATES_RESPONSE_NONE));
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(0, result.Dates.Count());
+            });
         }
     }
 }
