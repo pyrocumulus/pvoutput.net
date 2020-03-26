5a83d7e8132c787038c0b275a97002fd7f7cda74
Marcel <dev@mboersma.nl>
2020-03-26 22:51:58 +0100

    Final constructor clean up
    At the cost of read only properties, the constructors are easier to understand and maintain now
    Implemented lazy Logger property
    Added unit tests for the constructors, using NSubstitute

--- src/PVOutput.Net/PVOutputClient.cs
+++ src/PVOutput.Net/PVOutputClient.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Dawn;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using PVOutput.Net.DependencyInjection;
 using PVOutput.Net.Modules;
@@ -27,7 +28,18 @@ namespace PVOutput.Net
             } 
         }
 
-        internal ILogger Logger { get; }
+        private ILogger<PVOutputClient> _logger;
+        internal ILogger<PVOutputClient> Logger
+        {
+            get
+            {
+                return _logger ?? (_logger = NullLogger<PVOutputClient>.Instance);
+            }
+            set
+            {
+                _logger = value;
+            }
+        }
 
         /// <summary>ApiKey to use with authenticating.</summary>
         public string ApiKey { get; set; }
@@ -42,75 +54,79 @@ namespace PVOutput.Net
         /// The output service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getoutput">API information</see>.</para>
         /// </summary>
-        public OutputService Output { get; }
+        public OutputService Output { get; private set; }
 
         /// <summary>
         /// The system service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getsystem">API information</see>.</para>
         /// </summary>
-        public SystemService System { get; }
+        public SystemService System { get; private set; }
 
         /// <summary>
         /// The status service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getstatus">API information</see>.</para>
         /// </summary>
-        public StatusService Status { get; }
+        public StatusService Status { get; private set; }
 
         /// <summary>
         /// The statistic service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getstatistic">API information</see>.</para>
         /// </summary>
-        public StatisticsService Statistics { get; }
+        public StatisticsService Statistics { get; private set; }
 
         /// <summary>
         /// The missing service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getmissing">API information</see>.</para>
         /// </summary>
-        public MissingService Missing { get; }
+        public MissingService Missing { get; private set; }
 
         /// <summary>
         /// The team service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getsupply">API information</see>.</para>
         /// </summary>
-        public TeamService Team { get; }
+        public TeamService Team { get; private set; }
 
         /// <summary>
         /// The extended service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getextended">API information</see>.</para>
         /// </summary>
-        public ExtendedService Extended { get; }
+        public ExtendedService Extended { get; private set; }
 
         /// <summary>
         /// The favourite service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getfavourite">API information</see>.</para>
         /// </summary>
-        public FavouriteService Favourite { get; }
+        public FavouriteService Favourite { get; private set; }
 
         /// <summary>
         /// The insolation service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getinsolation">API information</see>.</para>
         /// </summary>
-        public InsolationService Insolation { get; }
+        public InsolationService Insolation { get; private set; }
 
         /// <summary>
         /// The supply service.
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-getsupply">API information</see>.</para>
         /// </summary>
-        public SupplyService Supply { get; }
+        public SupplyService Supply { get; private set; }
 
         /// <summary>
         /// The search service
         /// <para>See the official <see href="https://pvoutput.org/help.html#api-search">API information</see>.</para>
         /// </summary>
-        public SearchService Search { get; }
+        public SearchService Search { get; private set; }
 
         /// <summary>
         /// Creates a new PVOutputClient.
         /// </summary>
         /// <param name="apiKey">ApiKey to use with authenticating.</param>
         /// <param name="ownedSystemId">Id of the currently owned system used for authenticating.</param>
-        public PVOutputClient(string apiKey, int ownedSystemId) : this(apiKey, ownedSystemId, null)
-        {
+        public PVOutputClient(string apiKey, int ownedSystemId)
+        {            
+            ApiKey = apiKey;
+            OwnedSystemId = ownedSystemId;
+
+            CreateServices();
         }
 
         /// <summary>
@@ -119,12 +135,39 @@ namespace PVOutput.Net
         /// <param name="apiKey">ApiKey to use with authenticating.</param>
         /// <param name="ownedSystemId">Id of the currently owned system used for authenticating.</param>
         /// <param name="logger">The ILogger implementation, used for logging purposes.</param>
-        public PVOutputClient(string apiKey, int ownedSystemId, ILogger<PVOutputClient> logger)
+        public PVOutputClient(string apiKey, int ownedSystemId, ILogger<PVOutputClient> logger) : this(apiKey, ownedSystemId)
         {
-            ApiKey = apiKey;
-            OwnedSystemId = ownedSystemId;
-            Logger = logger ?? NullLogger<PVOutputClient>.Instance;
+            Logger = logger;
+        }
 
+        /// <summary>
+        /// Creates a new PVOutputClient.
+        /// </summary>
+        /// <param name="options">Options to use for the client, containing the ApiKey and Id of the owned system.</param>
+        public PVOutputClient(PVOutputClientOptions options)
+        {
+            Guard.Argument(options).NotNull();
+
+#pragma warning disable CA1062 // Validate arguments of public methods
+            ApiKey = options.ApiKey;
+            OwnedSystemId = options.OwnedSystemId;
+#pragma warning restore CA1062 // Validate arguments of public methods
+
+            CreateServices();
+        }
+
+        /// <summary>
+        /// Creates a new PVOutputClient, with a ILogger attached.
+        /// </summary>
+        /// <param name="options">Options to use for the client, containing the ApiKey and Id of the owned system.</param>
+        /// <param name="logger">The ILogger implementation, used for logging purposes.</param>
+        public PVOutputClient(PVOutputClientOptions options, ILogger<PVOutputClient> logger) : this(options)
+        {
+            Logger = logger;
+        }
+
+        private void CreateServices()
+        {
             Output = new OutputService(this);
             System = new SystemService(this);
             Status = new StatusService(this);
@@ -136,23 +179,6 @@ namespace PVOutput.Net
             Insolation = new InsolationService(this);
             Supply = new SupplyService(this);
             Search = new SearchService(this);
-        }
-
-        /// <summary>
-        /// Creates a new PVOutputClient, with a ILogger attached.
-        /// </summary>
-        /// <param name="options">Options to use for the client, containing the ApiKey and Id of the owned system.</param>
-        public PVOutputClient(PVOutputClientOptions options) : this(options?.ApiKey, options.OwnedSystemId, null)
-        {
-        }
-
-        /// <summary>
-        /// Creates a new PVOutputClient, with a ILogger attached.
-        /// </summary>
-        /// <param name="options">Options to use for the client, containing the ApiKey and Id of the owned system.</param>
-        /// <param name="logger">The ILogger implementation, used for logging purposes.</param>
-        public PVOutputClient(PVOutputClientOptions options, ILogger<PVOutputClient> logger) : this(options?.ApiKey, options.OwnedSystemId, logger)
-        {
         }
     }
 }
---
+++ tests/PVOutput.Net.Tests/Client/PVOutputClientTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using NUnit.Framework;
+using PVOutput.Net.DependencyInjection;
+
+namespace PVOutput.Net.Tests.Client
+{
+    [TestFixture]
+    public class PVOutputClientTests
+    {
+        [Test]
+        public void Create_ClientWithValidParameters_CreatesCorrectClient()
+        {
+            var client = new PVOutputClient("apikey", 1234);
+
+            Assert.Multiple(() => 
+            {
+                Assert.AreEqual("apikey", client.ApiKey);
+                Assert.AreEqual(1234, client.OwnedSystemId);
+                Assert.AreEqual(NullLogger<PVOutputClient>.Instance, client.Logger);
+            });
+        }
+
+        [Test]
+        public void Create_ClientWithLogger_CreatesClientWithLogger()
+        {
+            var loggerSubstitute = Substitute.For<ILogger<PVOutputClient>>();
+            var client = new PVOutputClient("apikey", 1234, loggerSubstitute);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("apikey", client.ApiKey);
+                Assert.AreEqual(1234, client.OwnedSystemId);
+                Assert.AreNotEqual(NullLogger<PVOutputClient>.Instance, client.Logger);
+            });
+        }
+
+
+        [Test]
+        public void Create_ClientWithValidOptions_CreatesCorrectClient()
+        {
+            var client = new PVOutputClient(new PVOutputClientOptions() { ApiKey = "apikey", OwnedSystemId = 1234 });
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("apikey", client.ApiKey);
+                Assert.AreEqual(1234, client.OwnedSystemId);
+            });
+        }
+
+        [Test]
+        public void Create_ClientWithOptionsAndLogger_CreatesClientWithLogger()
+        {
+            var loggerSubstitute = Substitute.For<ILogger<PVOutputClient>>();
+            var client = new PVOutputClient(new PVOutputClientOptions() { ApiKey = "apikey", OwnedSystemId = 1234 }, loggerSubstitute);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("apikey", client.ApiKey);
+                Assert.AreEqual(1234, client.OwnedSystemId);
+                Assert.AreNotEqual(NullLogger<PVOutputClient>.Instance, client.Logger);
+            });
+        }
+    }
+}
