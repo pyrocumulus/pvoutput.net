06e26698df9b85ab7d74c37be1ff7cd49fd13e25
Marcel <dev@mboersma.nl>
2020-10-08 21:51:41 +0200

    Improved coverage for StringFactoryContainer

--- tests/PVOutput.Net.Tests/Handler/OtherObjectTests.cs
+++ tests/PVOutput.Net.Tests/Handler/OtherObjectTests.cs
@@ -5,6 +5,9 @@ using System.Text;
 using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 using NUnit.Framework;
 using PVOutput.Net.Objects;
+using PVOutput.Net.Objects.Core;
+using PVOutput.Net.Objects.Factories;
+using PVOutput.Net.Objects.Modules.Readers;
 
 namespace PVOutput.Net.Tests.Handler
 {
@@ -89,6 +92,7 @@ namespace PVOutput.Net.Tests.Handler
         {
             return configuration1 == configuration2;
         }
+
         public static IEnumerable ExtendedDataConfigurationInvertedEqualityTestCases
         {
             get
@@ -104,6 +108,36 @@ namespace PVOutput.Net.Tests.Handler
         public bool ExtendedDataConfiguration_OperatorNotEquals_ReturnsValueEquality(ExtendedDataConfiguration configuration1, ExtendedDataConfiguration configuration2)
         {
             return configuration1 != configuration2;
+        }
+
+        [Test]
+        public void StringFactoryContainer_ForType_CreatesObjectReader()
+        {
+            IObjectStringReader<IStatus> reader = StringFactoryContainer.CreateObjectReader<IStatus>();
+            Assert.That(reader, Is.TypeOf<StatusObjectStringReader>());
+        }
+
+        [Test]
+        public void StringFactoryContainer_ForType_CreatesArrayReader()
+        {
+            IArrayStringReader<ISystemSearchResult> reader = StringFactoryContainer.CreateArrayReader<ISystemSearchResult>();
+            Assert.That(reader, Is.TypeOf<LineDelimitedArrayStringReader<ISystemSearchResult>>());
+        }
+
+        [Test]
+        public void StringFactoryContainer_ForUnknownType_Throws()
+        {
+            Assert.Throws<InvalidOperationException>(() => {
+                _ = StringFactoryContainer.CreateObjectReader<IServiceProvider>();
+            });
+        }
+
+        [Test]
+        public void StringFactoryContainer_ForTypeWithoutArrayReader_Throws()
+        {
+            Assert.Throws<InvalidOperationException>(() => {
+                _ = StringFactoryContainer.CreateArrayReader<IStatus>();
+            });
         }
     }
 }
